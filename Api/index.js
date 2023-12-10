ca7ba12ab035e9943af9bc50171908bc53249b9b
Amaan ansari <amaan.ansari2080@gmail.com>
2023-12-10 20:59:45 +0530

    created middleware and error handler

--- Api/index.js
+++ Api/index.js
@@ -29,3 +29,13 @@ app.use('/api/signup',authRouter);
 app.listen(3000,()=>{
     console.log('sever is runing on port 3000');
 });
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'internal server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
