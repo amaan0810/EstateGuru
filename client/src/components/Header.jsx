282f21873ecb82d0e391e133e116debaae618332
Amaan ansari <amaan.ansari2080@gmail.com>
2023-12-22 15:37:00 +0530

    updated css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,22 +4,22 @@ import {Link} from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-white shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>Estate</span>
-          <span className='text-slate-700'>Guru</span>
+          <span className='text-blue-500'>Estate</span>
+          <span className='text-blue-900'>Guru</span>
         </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input className='bg-transparent focus:outline-none w-24 sm:w-64' type='text' placeholder='Search...' />
           <FaSearch className='text-slate-600' />
         </form>
-        <ul className='flex gap-4'>
-          <Link to="/"><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
-          <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-          <Link to='/signin'><li className=' sm:inline text-slate-700 hover:underline'>Sign  in</li></Link>
+        <ul className='flex gap-4 font-medium'>
+          <Link to="/"><li className='hidden sm:inline text-blue-900 hover:underline'>Home</li></Link>
+          <Link to='/about'><li className='hidden sm:inline text-blue-900 hover:underline'>About</li></Link>
+          <Link to='/signin'><li className=' sm:inline text-blue-900 hover:underline'>Sign  in</li></Link>
         </ul>
       </div>
     </header>
