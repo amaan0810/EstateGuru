c4a43fd09db0f3fcdf221fefeb9d3333d0b3ec72
Amaan ansari <amaan.ansari2080@gmail.com>
2024-11-17 14:20:40 +0530

    final chages for deployment

--- Api/index.js
+++ Api/index.js
@@ -1,47 +1,55 @@
-import express from 'express';
-const app=express();
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+const __dirname = path.resolve();
+
+const app = express();
 
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("connected to MongoDB")
-})
-.catch((err)=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello " + req.cookies.access_token);
 });
 
+app.use("/api/user", userRouter);
 
-app.get('/',(req,res)=>{
-    res.send('Hello '+req.cookies.access_token);
-})
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-
-
-app.use('/api/user',userRouter);
-
-app.use('/api/auth',authRouter);
-app.use('/api/listing',listingRouter);
-
-
-app.listen(3000,()=>{
-    console.log('sever is runing on port 3000');
+app.listen(3000, () => {
+  console.log("sever is runing on port 3000");
 });
 
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'internal server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
